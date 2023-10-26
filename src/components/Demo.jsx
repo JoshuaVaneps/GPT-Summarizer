03eff432c626968faeb9eeee2b832adff9a16247
Joshua Vaneps <joshuavaneps@yahoo.com>
2023-10-26 01:00:46 -0400

    article summary showing and stylized

--- src/components/Demo.jsx
+++ src/components/Demo.jsx
@@ -14,7 +14,7 @@ const Demo = () => {
 
   const [getSummary, { error, isFetching }] = useLazyGetSummaryQuery();
 
-  // storign searches in local storage
+  // storing searches in local storage
   useEffect(() => {
     const articlesFromLocalStorage = JSON.parse(
       localStorage.getItem("articles")
@@ -28,10 +28,15 @@ const Demo = () => {
   const handleSubmit = async (e) => {
     e.preventDefault();
 
+    const existingArticle = allArticles.find(
+      (item) => item.url === article.url
+    );
+
+    if (existingArticle) return setArticle(existingArticle);
+
     const { data } = await getSummary({
       articleUrl: article.url,
     });
-
     if (data?.summary) {
       const newArticle = { ...article, summary: data.summary };
       // pushing new article into our all article array
@@ -39,9 +44,8 @@ const Demo = () => {
 
       setArticle(newArticle);
       setAllArticles(updatedAllArticles);
-
       // have to strignify for local storage
-      localStorage.setItem("article", JSON.stringify(updatedAllArticles));
+      localStorage.setItem("articles", JSON.stringify(updatedAllArticles));
     }
   };
 
@@ -77,11 +81,11 @@ const Demo = () => {
         </form>
         {/*Display Url History */}
         <div className="flex flex-col gap-1 max-h-60 overflow-y-auto">
-          {allArticles.map((item, index) => (
+          {allArticles.reverse().map((item, index) => (
             <div
-              className="link_card"
               key={`link-${index}`}
               onClick={() => setArticle(item)}
+              className="link_card"
             >
               <div className="copy_btn">
                 <img
@@ -99,6 +103,33 @@ const Demo = () => {
       </div>
 
       {/* Display Results */}
+      <div className="my-10 max-w-full flex justify-center items-center">
+        {isFetching ? (
+          <img src={loader} alt="loader" className="w-20 h-20 object-contain" />
+        ) : error ? (
+          <p className="font-inner font-bold text-black text-center">
+            that wasnt supoposed to happen..
+            <br />
+            <span className="font-satoshi font-normal text-gray-700">
+              {" "}
+              {error?.data?.error}
+            </span>
+          </p>
+        ) : (
+          article.summary && (
+            <div className="flex flex-col gap-3">
+              <h2 className="font-satoshi font-bold text-gray-600 text-xl">
+                Article <span className="blue_gradient">Summary</span>
+              </h2>
+              <div className="summary_box">
+                <p className="font-inter font-medium text-sm text-gray-700">
+                  {article.summary}
+                </p>
+              </div>
+            </div>
+          )
+        )}
+      </div>
     </section>
   );
 };
